019c3af2a24846af672d64e53a2c2e2fa9154994
theodoros.minaidis <theodoros.minaidis@peoplecert.org>
2024-12-31 14:26:04 +0200

    minaidis -> Add everything on Frontend folder and create a start.bat to start the app

---
+++ start.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+:: Start the API server
+:: start "API Server" cmd /k "cd /d backend\ServerAPI && npm start"
+
+dir
+:: Start the website
+start "Website" cmd /k "cd /d Frontend && npm start"
+
+:: Build the website
+cd /d Frontend
+npm run build
+
+endlocal
